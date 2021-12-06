d8f99ecf1c87474c46c0bb09a9ddd85466b5eed5
Chupols0920 <joshualagan099@gmail.com>
2021-12-06 16:38:21 +0800

    material icon next

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -2,7 +2,7 @@ import "./contacts.scss";
 
 export default function Contacts() {
     return (
-        <div className="contact">
+        <div className="contact" id="contact">
 
         </div>
     )
--- src/Components/TopBar/Topbar.jsx
+++ src/Components/TopBar/Topbar.jsx
@@ -3,7 +3,14 @@ import "./topbar.scss";
 export default function Topbar() {
     return (
         <div className="topbar">
-
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">BOOMY</a>
+                </div>
+                <div className="right">
+                    right
+                </div>
+            </div>
         </div>
     )
 }
